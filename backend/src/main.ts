79593f502ece1b5b8989f9dc6028bcd8623e858c
Vishal Tiwari <vishaltiwari642@gmail.com>
2025-08-06 14:07:56 +0530

    main.ts file update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'https://money-splitter-app-iota.vercel.app',
+    origin: 'https://money-splitter-app-iota.vercel.app', // your frontend on Vercel
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
